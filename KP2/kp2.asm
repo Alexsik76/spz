125c62a79fd1469cefff2487ce5d5d4ab631391b
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2024-04-08 01:14:46 +0300

    first commit

---
+++ KP2/kp2.asm
@@ -0,0 +1,57 @@
+STSEG SEGMENT PARA STACK "STACK"
+          DB 32 DUP(?)
+STSEG ENDS
+DSEG SEGMENT PARA PUBLIC "DATA"
+    MESSTR1 DB     "Enter number ->$"
+    MESSTR2 DB     "Number is negative$"
+    MESSTR3 DB     "Number is positive$"
+            NUMPAR LABEL    BYTE
+    MAXLEN  DB     6
+    NUBLEN  DB     ?
+    NUMFLD  DB     6 DUP('0')
+DSEG ENDS
+CSEG SEGMENT PARA PUBLIC "CODE"
+MAIN PROC FAR
+         ASSUME CS: CSEG, DS: DSEG, SS: STSEG    ;
+         PUSH   DS                               ; init
+         XOR    AX, AX                           ;
+         PUSH   AX                               ;
+    
+    ; print request
+         MOV    AX, DSEG
+         MOV    DS, AX
+         LEA    DX, MESSTR1
+         MOV    AH, 9
+         INT    21h
+    
+    ; get number
+         LEA    DX, NUMPAR
+         MOV    AH, 10
+         INT    21h
+
+    ; check is negative
+         CLD
+         LEA    SI, NUMFLD
+         LODSB
+         CMP    AL, '-'
+         JE     PNG
+         JNE    PPOS
+E90:     RET
+MAIN ENDP
+
+PPOS PROC NEAR                                   ; print positive
+         LEA    DX, MESSTR3
+         MOV    AH, 9
+         INT    21h
+         JMP E90
+PPOS ENDP
+
+PNG PROC NEAR                                    ; print negative
+         LEA    DX, MESSTR2
+         MOV    AH, 9
+         INT    21h
+         JMP E90
+PNG ENDP
+
+CSEG ENDS
+     END MAIN
