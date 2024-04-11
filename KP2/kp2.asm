03f55849d416cb4f8475347a98e33eb878047fd4
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2024-04-12 02:09:08 +0300

    rework by lection

--- KP2/kp2.asm
+++ KP2/kp2.asm
@@ -1,14 +1,15 @@
 STSEG SEGMENT PARA STACK "STACK"
-              DB 32 DUP(?)
+              DB 256 DUP(?)
 STSEG ENDS
 DSEG SEGMENT PARA PUBLIC "DATA"
-        CR      EQU    13                             ;или EQU 0DH
-        LF      EQU    10                             ;или EQU 0AH
-        TAB     EQU    09                             ;или EQU 09H
+        CR      EQU    13                              ;или EQU 0DH
+        LF      EQU    10                              ;или EQU 0AH
         MESSTR1 DB     "Enter number: $"
         MESSERR DB     CR, LF, "Not number!$"
+        MESS1 DB     CR, LF, "Value to big!$"
         SIGN    DB     0
-        MULT10  DB     1
+        ERRVAL  DB     0
+        MULT10  DW     1
         BINVAL  DW     0
         ASCVAL  DB     ?
         TOMUL   DW     7
@@ -22,139 +23,132 @@ DSEG SEGMENT PARA PUBLIC "DATA"
 DSEG ENDS
 CSEG SEGMENT PARA PUBLIC "CODE"
 MAIN PROC FAR
-                ASSUME CS: CSEG, DS: DSEG, SS: STSEG, ES: DSEG
-                PUSH   DS
-                XOR    AX, AX
-                PUSH   AX
-                MOV    AX, DSEG
-                MOV    DS, AX
-                MOV    ES, AX
-                CALL   GTINPT
-                CALL   CHKSIGN
-                CALL   CHKNMBR
-                JZ     E10
-                CALL   ASBI
-                MOV    AX, BINVAL
-                MUL    TOMUL
-                LEA    BX, RESMUL
-                MOV    [BX], AX
-                CALL   BIAS
-                LEA    DX, RESASC
-                CALL   PRNMSG
-        E10:    RET
+                 ASSUME CS: CSEG, DS: DSEG, SS: STSEG
+                 PUSH   DS
+                 XOR    AX, AX
+                 PUSH   AX
+                 MOV    AX, DSEG
+                 MOV    DS, AX
+                 MOV    ES, AX
+                 CALL   GTINPT
+                 CALL   CHKSIGN
+                 CALL   ASBI
+                 CMP    ERRVAL, 1
+                 JE     E10
+                 MOV    AX, BINVAL
+                 MUL    TOMUL
+                 JC     TOBIG
+                 CMP    ERRVAL, 1
+                 JE     E10
+                 LEA    BX, RESMUL
+                 MOV    [BX], AX
+                 CALL   BIAS
+                 LEA    DX, RESASC
+        E10:     CALL   PRNMSG
+                 RET
 MAIN ENDP
 
-GTINPT PROC NEAR                                                      ; get input
-                LEA    DX, MESSTR1
-                CALL   PRNMSG
-                LEA    DX, NUMPAR
-                MOV    AH, 10
-                INT    21h
-                RET
+GTINPT PROC NEAR                                             ; get input
+                 LEA    DX, MESSTR1
+                 CALL   PRNMSG
+                 LEA    DX, NUMPAR
+                 MOV    AH, 10
+                 INT    21h
+                 RET
 GTINPT ENDP
 
-CHKSIGN PROC NEAR                                                     ; check is negative
-                CLD
-                LEA    DI, NUMFLD
-                MOV    AL, '-'
-                SCASB
-                JNE    PPOS
-                JE     PNG
-                RET
+CHKSIGN PROC NEAR                                            ; check is negative
+                 CLD
+                 LEA    DI, NUMFLD
+                 MOV    AL, [DI]
+                 CMP    AL, 2dh
+                 JNE    Z11
+                 MOV    SIGN, 1
+                 jmp    Z12
+        Z11:     MOV    SIGN, 0
+        Z12:     ret
 CHKSIGN ENDP
 
-CHKNMBR PROC NEAR                                                     ; check is number
-                CLD
-                LEA    DI, NUMFLD
-                MOV    CL, NUMLEN
-                MOV    AH,0
-                MOV    AL, SIGN
-                ADD    DI, AX
-                SUB    CL, AL
-        B20:    
-                MOV    AL, [DI]
-                CMP    AL, 48
-                JB     PNN
-                CMP    AL, 57
-                JA     PNN
-                INC    DI
-                LOOP   B20
-        B30:    RET
-CHKNMBR ENDP
-
-PRNMSG PROC NEAR                                                      ; print message
-                MOV    AH, 9
-                INT    21h
-                RET
+PRNMSG PROC NEAR                                             ; print message
+                 MOV    AH, 9
+                 INT    21h
+                 RET
 PRNMSG ENDP
 
-PNN PROC NEAR                                                         ; not number
-                LEA    DX, MESSERR
-                CALL   PRNMSG
-                XOR    BX, BX
-                JMP    B30
-                RET
-                
+CHECKNUM PROC NEAR
+                 CMP    AL, 48
+                 JB     PNN
+                 CMP    AL, 57
+                 JA     PNN
+                 RET
+CHECKNUM ENDP
+
+PNN PROC NEAR                                                ; not number
+                 LEA    DX, MESSERR
+                 MOV    ERRVAL, 1
+                 RET
 PNN ENDP
 
-PPOS PROC NEAR                                                        ; positive
-                MOV    SIGN, 0
-                RET
-PPOS ENDP
+TOBIG PROC NEAR                                                ; to big number
+                 LEA    DX, MESS1
+                 MOV    ERRVAL, 1
+                 RET
+TOBIG ENDP
 
-PNG PROC NEAR                                                         ; negative
-                MOV    SIGN, 1
-                RET
-PNG ENDP
 
-ASBI PROC NEAR                                                        ; convert ASCII to bin
-                MOV    CX, 10
-                LEA    SI, NUMFLD-1
-                XOR    AX, AX
-                XOR    BX, BX
-
-                MOV    AL, SIGN
-                ADD    SI, AX
-                MOV    BL, NUMLEN
-                SUB    BL, SIGN
-                LEA    AX,BINVAL
-        C30:    
-                MOV    AL, [SI+BX]
-                AND    AX, 000FH
-                MUL    MULT10
-                ADD    BINVAL, AX
-                MOV    AL, MULT10
-                MUL    CX
-                MOV    MULT10, AL
-                DEC    BX
-                JNZ    C30
-                RET
+ASBI PROC NEAR                                               ; convert ASCII to bin
+                 XOR    CX, CX
+                 MOV    CL, NUMLEN
+                 LEA    SI, NUMFLD-1                         ; to get in 101 the last symbol addr
+                 XOR    AX, AX
+                 XOR    BX, BX
+                 XOR    DX, DX
+                 MOV    DL, SIGN
+                 ADD    SI, DX
+                 SUB    CL, SIGN
+        C30:     
+                 MOV    BX, CX
+                 MOV    AL, [SI+BX]
+                 CALL   CHECKNUM
+                 CMP    ERRVAL, 1
+                 JE     C50
+                 SUB    AL, '0'
+                 MUL    MULT10
+                 ADD    BINVAL, AX
+                 JC     TOBIG
+                 CMP    ERRVAL, 1
+                 JE     C50
+                 MOV    AX, 10
+                 MUL    MULT10
+                 MOV    MULT10, AX
+                 XOR    AX, AX
+                 LOOP   C30
+        C50:     RET
 ASBI ENDP
-BIAS PROC
+BIAS PROC                                                    ; convert bin to ASCII
+                 MOV    CX,0010
+                 LEA    SI,RESASC+7
+                 MOV    AX,RESMUL
                 
-                MOV    CX,0010                                        ;Фактор деления
-                LEA    SI,RESASC+7                                    ;Адрес ASCVAL
-                MOV    AX,RESMUL                                      ;Загрузить дв. число
-                
-        D20:    
-                CMP    AX,10                                          ;Значение меньше 10?
-                JB     D30                                            ; Да - выйти
-                XOR    DX,DX                                          ;Очистить часть частного
-                DIV    CX                                             ;Разделить на 10
-                OR     DL,30H
-                MOV    [SI],DL                                        ;Записать ASCII-символ
-                DEC    SI
-                JMP    D20
-        D30:    
-                OR     AL,30H                                         ;3аписать поcл. частное
-                MOV    [SI],AL                                        ; как ASCII-символ
-                CMP    SIGN, 1
-                JE     D40
-                JMP    D50
-        D40:    DEC    SI
-                MOV    DL, 2DH
-                MOV    [SI],DL
-        D50:    RET
+        D20:     
+                 CMP    AX,10
+                 JB     D30
+                 XOR    DX,DX
+                 DIV    CX
+                 OR     DL,30H
+                 MOV    [SI],DL
+                 DEC    SI
+                 JMP    D20
+        D30:     
+                 OR     AL,30H
+                 MOV    [SI],AL
+                 CMP    SIGN, 1
+                 JE     D40
+                 JMP    D50
+        D40:     DEC    SI
+                 MOV    DL, 2DH
+                 MOV    [SI],DL
+        D50:     RET
 BIAS ENDP
 
 CSEG ENDS
--- kp21/kp2.asm
+++ kp21/kp2.asm
@@ -2,11 +2,11 @@ STSEG SEGMENT PARA STACK "STACK"
           DB 32 DUP(?)
 STSEG ENDS
 DSEG SEGMENT PARA PUBLIC "DATA"
-    inputPrompt db 'Введіть ціле число: $'
-    errorMsg    db 'Помилка введення. Спробуйте ще раз.$'
-    resultMsg   db 'Результат: $'
-    inputBuffer db 6                                         ; Максимальна довжина числа + знак нового рядка
-    number      dw ?
+    inputPrompt db 'Enter number: $'
+    errorMsg    db 13, 10,'Error. Try again.$'
+    resultMsg   db 13, 10, 'Result:', 13, 10,  '$'
+    inputBuffer db 6                                  ; Максимальна довжина числа + знак нового рядка
+    number      dw 0
 
 DSEG ENDS
 CSEG SEGMENT PARA PUBLIC "CODE"
@@ -28,7 +28,7 @@ MAIN PROC FAR
                     jc     inputNumber                                ; Якщо сталася помилка, повторити запит
 
     ; Виконання арифметичної операції
-                    ; sub    number, 23
+                    sbb    number, 11
 
     ; Виведення результату
                     lea    dx, resultMsg
@@ -56,6 +56,7 @@ readInteger endp
 stringToInteger proc near
                     xor    ax, ax                                     ; AX = 0
                     xor    cx, cx                                     ; Скидання CX для використання в якості лічильника
+                    xor    Dx, Dx                                     ; Скидання CX для використання в якості лічильника
                     mov    cl, byte ptr [si]                          ; Довжина рядка
                     inc    si                                         ; Перехід до першого символу числа
     checkDigit:     
@@ -75,7 +76,7 @@ stringToInteger proc near
     ; Додавання цифри до числа
     ; MOV CH, 10
                     MOV    BL, 10
-                    IMUL   BL
+                    MUL    BL
                     add    ax, dx
                     inc    si
 
@@ -91,7 +92,7 @@ printInteger proc near
                     mov    ax, number
                     call   integerToString
                     mov    ah, 09h
-                    lea    dx, inputBuffer                         ; Використання inputBuffer як буфера для рядка
+                    lea    dx, inputBuffer                            ; Використання inputBuffer як буфера для рядка
                     int    21h
                     ret
 printInteger endp
@@ -100,12 +101,9 @@ printInteger endp
 integerToString proc near
                   
                     MOV    CX,0010
-                    LEA    SI,inputBuffer+5
-                    MOV    AX,number
-                    
-                
+                    LEA    SI,inputBuffer+6
     D20:            
-                    CMP    AX,0010
+                    CMP    AX,10
                     JB     D30
                     XOR    DX,DX
                     DIV    CX
